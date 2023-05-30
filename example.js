3575592f287f8d7114c77321453a03441e38227c
Otavio <suizld@pm.me>
2023-05-30 10:11:11 -0300

    update: renamed log and run methods to a more suiting name
    improved the examples inside: `example.js`.
    improved colors dictionary and added flags.
    added the ability to customize the sufix on `print` and `throw` functions.

--- example.js
+++ example.js
@@ -5,46 +5,75 @@
 
 import flog from "./lib/flog.mjs";
 
+/*
+ This is a good callback (will not throw an exception, all methods called inside it exists inside JavaScript)
+*/
 const goodCallback = (something1 = 12345678, something2 = 'AAAbbbCCCdddEEEfffGGG') => {
   something1.toString();
   something2.toLowerCase();
 };
+
+/*
+ This is a bad callback (will certainly cause an exception, the method called inside it does not exist inside JavaScript)
+*/
 const badCallback = (something1) => {
   something1.unknownFunction();
 };
-const testVariable = 123456;
+
+const variable1 = 123456;
+const variable2 = '123456';
 
 (() => {
-  // logging examples
-  flog.log('test message', '+');
-  flog.log('test message', '+', 'success');
-  flog.log('test message', '+', 'fatal');
-  flog.log('test message', '+', 'error');
-  flog.log('test message', '+', 'warning');
-  flog.log('test message', '+', 'info');
-  flog.log('test message', '+', 'normal');
-  // trace and callee info
+  /*
+   Log messages example
+  */
+  flog.print('test message', '+');
+  flog.print('test message', '+', 'success');
+  flog.print('test message', '+', 'fatal');
+  flog.print('test message', '+', 'error');
+  flog.print('test message', '+', 'warning');
+  flog.print('test message', '+', 'info');
+
+  /*
+   Get function/callback trace or function/callback callee information:
+  */
   {
-    // make sure to call these 2 functions after your callback is called
+    goodCallback();
+    /* If you are going to use one of these 2 functions, make sure to call them after your callback 
+    or function that you want to trace. */
     flog.trace();
     flog.callee();
   }
-  // get the typeof something
-  {
-    flog.type(goodCallback);
-    // flog.type(badCallback);
-    flog.type(testVariable);
-  }
-  // tcf wrapper
-  {
-    // good callback without debug logs
-    flog.run(goodCallback);
 
-    // bad callback with debug logs and if its throwable or not
-    // flog.run(badCallback, true, true); // thats part is commented out to avoid the 'test run' from being stopped
+  /*
+   Get the type of anything
+  */
+  {
+    flog.type(goodCallback); // should return `function`
+
+    flog.type(variable1); // should return `number`
+    flog.type(variable2); // should return `string`
   }
 
-  // throw examples
+  /*
+   Function/callback runner:
+   
+   You can safely execute a function that is not so safe here, flog will handle the exceptions
+   or if you want to log debug info while executing a function.
+  */
+  {
+    // flog will execute a good callback without debug logs
+    flog.execute(goodCallback);
+
+    // flog will execute a bad callback with debug logs and if it will thrown an exception or not
+
+    // Uncomment the line above if you want to see how flog handles bad callbacks.
+    // flog.execute(badCallback, true, true);
+  }
+
+  /*
+   Throw examples
+  */
   flog.throw('throw error message', '+', 'error', true); // this is a fake throw, the application won't stop
   flog.throw('throw fatal error message', '+', 'fatal'); // the 'test run' will stop here.
 })();
