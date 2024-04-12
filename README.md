80ad623bd20a5a2d1cb29b73ebad42fb6e6ced41
Otavio <suizld@pm.me>
2024-04-12 08:59:42 -0300

    update: renamed package/repo name
    Renamed package name to 'flogger' to avoid conflicts with another existing npm package.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# flog :frog:
+# flogger :frog:
 
 Logging and debugging made easier for your JavaScript applications.
 
 ***
 
-**flog** is available under the [MIT License](https://github.com/otvv/flog/blob/master/LICENSE)
+**flogger** is available under the [MIT License](https://github.com/otvv/flogger/blob/master/LICENSE)
--- example.js
+++ example.js
@@ -3,7 +3,7 @@
 
 'use strict'
 
-import flog from './lib/flog.mjs';
+import flogger from './lib/flogger.mjs';
 
 /*
  This is a good callback example (will not throw an exception, all methods called inside it exists inside JavaScript):
@@ -30,12 +30,12 @@ const variable2 = '123456';
   /*
    Log messages example:
   */
-  flog.print('test message', '+');
-  flog.print('test message', '+', 'success');
-  flog.print('test message', '+', 'fatal');
-  flog.print('test message', '+', 'error');
-  flog.print('test message', '+', 'warning');
-  flog.print('test message', '+', 'info');
+  flogger.print('test message', '+');
+  flogger.print('test message', '+', 'success');
+  flogger.print('test message', '+', 'fatal');
+  flogger.print('test message', '+', 'error');
+  flogger.print('test message', '+', 'warning');
+  flogger.print('test message', '+', 'info');
 
   /*
    Get function/callback trace or function/callback callee information:
@@ -44,39 +44,39 @@ const variable2 = '123456';
     goodCallback();
     /* If you are going to use one of these 2 functions, make sure to call them after your callback 
     or function that you want to trace. */
-    flog.trace();
-    flog.callee();
+    flogger.trace();
+    flogger.callee();
   }
 
   /*
    Get the type of anything:
   */
   {
-    flog.type(goodCallback); // should return `function`
+    flogger.type(goodCallback); // should return `function`
 
-    flog.type(variable1); // should return `number`
-    flog.type(variable2); // should return `string`
+    flogger.type(variable1); // should return `number`
+    flogger.type(variable2); // should return `string`
   }
 
   /*
    Function/callback runner:
    
-   You can safely execute a function that is not so safe here, flog will handle the exceptions
+   You can safely execute a function that is not so safe here, flogger will handle the exceptions
    or if you want to log debug info while executing a function.
   */
   {
-    // flog will execute a good callback without debug logs
-    flog.execute(goodCallback);
+    // flogger will execute a good callback without debug logs
+    flogger.execute(goodCallback);
 
-    // flog will execute a bad callback with debug logs and if it will thrown an exception or not
+    // flogger will execute a bad callback with debug logs and if it will thrown an exception or not
 
-    // Uncomment the line below if you want to see how flog handles bad callbacks.
-    // flog.execute(badCallback, true, true);
+    // Uncomment the line below if you want to see how flogger handles bad callbacks.
+    // flogger.execute(badCallback, true, true);
   }
 
   /*
    Throw examples:
   */
-  flog.throw('throw error message', '+', 'error', true); // this is a fake throw, the application won't stop
-  flog.throw('throw fatal error message', '+', 'fatal'); // the 'test run' will stop here.
+  flogger.throw('throw error message', '+', 'error', true); // this is a fake throw, the application won't stop
+  flogger.throw('throw fatal error message', '+', 'fatal'); // the 'test run' will stop here.
 })();
